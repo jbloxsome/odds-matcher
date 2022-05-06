335e26b0d529294c91430a18bdaf83ab6102b08c
Joe Bloxsome <joe@bloxso.me>
2022-05-06 20:47:11 +0100

    display odds in american format

--- frontend/src/components/molecules/opportunity.js
+++ frontend/src/components/molecules/opportunity.js
@@ -3,6 +3,14 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import './opportunity.css';
 
+function american(decimal) {
+    if (decimal >= 2) {
+        return '+' + Math.ceil((decimal - 1) * 100).toString()
+    } else {
+        return '-' + (-1 * Math.ceil(-100 / (decimal - 1))).toString()
+    }
+}
+
 function Opportunity({ opportunity }) {
     return(
         <Card>
@@ -19,21 +27,21 @@ function Opportunity({ opportunity }) {
                     <Col xs={2}>
                         <h5>Bet 1</h5>
                         <h6>Home Win - {opportunity.home_win_bookmaker}</h6>
-                        <p>Decimal Odds: {opportunity.home_win_price}</p>
+                        <p>Odds: {american(opportunity.home_win_price)}</p>
                         <p>Stake: £{opportunity.home_win_stake.toFixed(2)}</p>
                         <p>Profit: £{opportunity.home_win_profit.toFixed(2)}</p>
                     </Col>
                     <Col xs={2}>
                         <h5>Bet 2</h5>
                         <h6>Draw - {opportunity.draw_bookmaker}</h6>
-                        <p>Decimal Odds: {opportunity.draw_price}</p>
+                        <p>Odds: {american(opportunity.draw_price)}</p>
                         <p>Stake: £{opportunity.draw_stake.toFixed(2)}</p>
                         <p>Profit: £{opportunity.draw_profit.toFixed(2)}</p>
                     </Col>
                     <Col xs={2}>
                         <h5>Bet 3</h5>
                         <h6>Away Win - {opportunity.away_win_bookmaker}</h6>
-                        <p>Decimal Odds: {opportunity.away_win_price}</p>
+                        <p>Odds: {american(opportunity.away_win_price)}</p>
                         <p>Stake: £{opportunity.away_win_stake.toFixed(2)}</p>
                         <p>Profit: £{opportunity.away_win_profit.toFixed(2)}</p>
                     </Col>
